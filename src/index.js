b8f4817ff2e615c741a08b332d4becd5ebe17405
Jerko Steiner <jerko.steiner@gmail.com>
2016-04-17 21:29:32 -0400

    Add support for https

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ if (!process.env.DEBUG) {
 }
 
 const app = require('./server/app.js');
+const config = require('config');
 const debug = require('debug')('influx-query');
+const http = require('http');
+const https = require('https');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+let server;
+if (config.has('ssl.key') && config.has('ssl.crt')) {
+  server = https.createServer({
+    key: config.get('ssl.key'),
+    cert: config.get('ssl.crt')
+  }, app);
+} else {
+  server = http.createServer(app);
+}
+
+server.listen(port, () => {
   debug('Listening on port %s', port);
 });
